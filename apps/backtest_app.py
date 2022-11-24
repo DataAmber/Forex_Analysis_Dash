ab08792b3aeb441e5bfe5efc028500a9c5840c45
bean <bingblackbean@gmail.com>
2022-11-24 13:55:22 +0100

    initial homepage

---
+++ apps/backtest_app.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+"""
+@Project ：Forex_Analysis_Dash
+@File ：backtest_app.py
+@Author ： Bean
+@Date ：11/24/22 1:50 PM
+"""
+from dash import html
+
+bta_layout = html.H2('Backtest Page')
---
+++ apps/online_tracking_app.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+"""
+@Project ：Forex_Analysis_Dash
+@File ：online_tracking_app.py
+@Author ： Bean
+@Date ：11/24/22 1:50 PM
+"""
+from dash import html
+
+ota_layout = html.H2('Online Tracking')
