8636e602fbc734d00b7726e0128f7a6fc5044436
bean <bingblackbean@gmail.com>
2022-12-06 07:07:36 +0100

    using ts lib to calc macd and using pandas calc strategy

--- algrithms/util/MACD.py
+++ algrithms/util/MACD.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-
+from ta.trend import MACD
 
 """
 compute_macd：根据收盘价计算MACD
@@ -17,24 +17,22 @@ plot_macd：图中画出MACD line和Signal line
 implement_macd_strategy：根据MACD和Signal实现买入卖出策略
 plot_macd_with_bs：图中标记买入卖出信号
 """
+
+
 def compute_macd(df, slow, fast, smooth):
-    price = df["close"]
-    exp1 = price.ewm(span = fast, adjust = False).mean() # fast ema
-    exp2 = price.ewm(span = slow, adjust = False).mean() # slow ema
-    macd = pd.DataFrame(exp1 - exp2).rename(columns = {'close':'macd'})
-    signal = pd.DataFrame(macd.ewm(span = smooth, adjust = False).mean()).rename(columns = {'macd':'signal'}) # signal line
-    hist = pd.DataFrame(macd['macd'] - signal['signal']).rename(columns = {0:'hist'})
-    frames =  [df, macd, signal, hist]
-    df_res = pd.concat(frames, join = 'inner', axis = 1)
-    return df_res
+    macd_trend = MACD(df['close'], window_slow=slow, window_fast=fast, window_sign=smooth)
+    df['macd'] = macd_trend.macd()
+    df['signal'] = macd_trend.macd_signal()
+    df['hist'] = macd_trend.macd_diff()
+    return df
+
 
 def plot_macd(df):
     fig = make_subplots(rows=2, cols=1)
-
     fig.add_trace(
         go.Scatter(
-            name='收盘价', 
-            x=df['Date'], 
+            name='收盘价',
+            x=df['Date'],
             y=df['close']
         ),
         row=1, col=1
@@ -42,8 +40,8 @@ def plot_macd(df):
 
     fig.add_trace(
         go.Scatter(
-            name='MACD Line', 
-            x=df['Date'], 
+            name='MACD Line',
+            x=df['Date'],
             y=df['macd'],
             marker=dict(
                 color='red'
@@ -53,8 +51,8 @@ def plot_macd(df):
     )
     fig.add_trace(
         go.Scatter(
-            name='Signal Line', 
-            x=df['Date'], 
+            name='Signal Line',
+            x=df['Date'],
             y=df['signal'],
             marker=dict(
                 color='green'
@@ -64,90 +62,63 @@ def plot_macd(df):
     )
     fig.add_trace(
         go.Bar(
-            name='hist', 
-            x=df['Date'], 
+            name='hist',
+            x=df['Date'],
             y=df['hist'],
             marker=dict(
-                color = ['rgba(63, 195, 128, 1)' if v >0 else 'rgba(219, 10, 91, 1)' for v in df['hist']]
+                color=['rgba(63, 195, 128, 1)' if v > 0 else 'rgba(219, 10, 91, 1)' for v in df['hist']]
             )
         ),
         row=2, col=1
     )
-
     fig.update_layout(height=600, width=800, title_text="MACD and Signal Line")
     fig.show()
-    
-    
-def implement_macd_strategy(prices, data):    
-    buy_price = []
-    sell_price = []
-    macd_signal = []
-    signal = 0
 
-    for i in range(len(data)):
-        if data['macd'][i] > data['signal'][i]:  # 买入信号
-            if signal != 1:
-                buy_price.append(prices[i])
-                sell_price.append(np.nan)
-                signal = 1
-                macd_signal.append(signal)
-            else:
-                buy_price.append(np.nan)
-                sell_price.append(np.nan)
-                macd_signal.append(0)
-        elif data['macd'][i] < data['signal'][i]: # 卖出信号
-            if signal != -1:
-                buy_price.append(np.nan)
-                sell_price.append(prices[i])
-                signal = -1
-                macd_signal.append(signal)
-            else:
-                buy_price.append(np.nan)
-                sell_price.append(np.nan)
-                macd_signal.append(0)
-        else:
-            buy_price.append(np.nan)
-            sell_price.append(np.nan)
-            macd_signal.append(0)
-            
-    return buy_price, sell_price, macd_signal
+
+def implement_macd_strategy(data):
+    data['buy_price'] = data['close']
+    data['sell_price'] = data['close']
+    data['signal_sign'] = (data['macd'] > data['signal']).replace({True: 1, False: 0})
+    data['signal_sign_diff'] = data['signal_sign'].diff().fillna(0)
+    data.loc[data['signal_sign_diff'] != 1, 'buy_price'] = np.nan
+    data.loc[data['signal_sign_diff'] != -1, 'sell_price'] = np.nan
+    return data
 
 
 def plot_macd_with_bs(df):
     fig = make_subplots(rows=2, cols=1)
-
     fig.add_trace(
         go.Scatter(
-            name='收盘价', 
-            x=df['Date'], 
+            name='收盘价',
+            x=df['Date'],
             y=df['close']
         ),
         row=1, col=1
     )
     fig.add_trace(
         go.Scatter(
-            name='buy signal', 
-            x=df['Date'], 
+            name='buy signal',
+            x=df['Date'],
             y=df['buy_price'],
             mode="lines+markers",
             marker=dict(
-                color = 'green',
-                symbol = 'triangle-up',
-                size = 8,
+                color='green',
+                symbol='triangle-up',
+                size=8,
             )
         ),
         row=1, col=1
     )
     fig.add_trace(
         go.Scatter(
-            name='sell signal', 
-            x=df['Date'], 
+            name='sell signal',
+            x=df['Date'],
             y=df['sell_price'],
             mode="lines+markers",
             marker=dict(
-                color = 'red',
-                symbol = 'triangle-down',
-                size = 8,
+                color='red',
+                symbol='triangle-down',
+                size=8,
             )
         ),
         row=1, col=1
@@ -155,8 +126,8 @@ def plot_macd_with_bs(df):
 
     fig.add_trace(
         go.Scatter(
-            name='MACD Line', 
-            x=df['Date'], 
+            name='MACD Line',
+            x=df['Date'],
             y=df['macd'],
             marker=dict(
                 color='red'
@@ -166,8 +137,8 @@ def plot_macd_with_bs(df):
     )
     fig.add_trace(
         go.Scatter(
-            name='Signal Line', 
-            x=df['Date'], 
+            name='Signal Line',
+            x=df['Date'],
             y=df['signal'],
             marker=dict(
                 color='green'
@@ -177,11 +148,11 @@ def plot_macd_with_bs(df):
     )
     fig.add_trace(
         go.Bar(
-            name='hist', 
-            x=df['Date'], 
+            name='hist',
+            x=df['Date'],
             y=df['hist'],
             marker=dict(
-                color = ['rgba(63, 195, 128, 1)' if v >0 else 'rgba(219, 10, 91, 1)' for v in df['hist']]
+                color=['rgba(63, 195, 128, 1)' if v > 0 else 'rgba(219, 10, 91, 1)' for v in df['hist']]
             )
         ),
         row=2, col=1
@@ -190,18 +161,12 @@ def plot_macd_with_bs(df):
     fig.update_layout(height=600, width=800, title_text="MACD and Signal Line")
     fig.show()
 
-if "__main__"==__name__:
-    data_path = r'algrithms\data\BTC_USD.csv'
+
+if "__main__" == __name__:
+    data_path = r'../data/BTC_USD.csv'
     df = pd.read_csv(data_path)
-    
     df = df.dropna(axis=0, how='any')
     df = df.reset_index(drop=True)
-    
     df_macd = compute_macd(df, 26, 12, 9)
-    
-    buy_price, sell_price, macd_signal = implement_macd_strategy(df_macd['close'], df_macd)
-    df_macd['buy_price'] = buy_price
-    df_macd['sell_price'] = sell_price
-    df_macd['macd_signal'] = macd_signal
-
+    df_macd = implement_macd_strategy(df_macd)
     plot_macd_with_bs(df_macd)
